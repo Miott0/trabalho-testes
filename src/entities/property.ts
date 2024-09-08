210a08415f7449a58dbaf075b4e7de0b5cd64049
Maycon <maycon.fernandes9@gmail.com>
2024-09-08 16:49:50 -0300

    fix proprety

--- src/entities/property.ts
+++ src/entities/property.ts
@@ -1,11 +1,11 @@
 import { IProperty } from "../interface/IProperty";
 
 export class Property implements IProperty {
-    id?: number;
-    area?: number;
-    address?: string;
+    id: number;
+    area: number;
+    address: string;
 
-    constructor(id?: number, area?: number, address?: string) {
+    constructor(id: number, area: number, address: string) {
         this.id = id;
         this.area = area;
         this.address = address;
