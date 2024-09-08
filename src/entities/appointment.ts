17bb8342fe1ff877f255dd24c1f4e129c2665ec4
Maycon <maycon.fernandes9@gmail.com>
2024-09-08 16:46:15 -0300

    fix appointment

--- src/entities/appointment.ts
+++ src/entities/appointment.ts
@@ -1,20 +1,24 @@
 import { IAppointment } from "../interface/IAppointment";
 
 export class Appointment implements IAppointment {
-    id?: number;
+    id: number;
     title?: string;
-    startDate?: Date; 
-    endDate?: Date;
-    idUser?: number;
-    idProperty?: number;
+    startDate: Date; 
+    endDate: Date;
+    idUser: number;
+    idProperty: number;
+    createdAt?: Date;
+    updatedAt?: Date;
 
-    constructor(id?: number, title?: string, startDate?: Date, endDate?: Date, idUser?: number, idProperty?: number) {
+    constructor(id: number, title: string, startDate: Date, endDate: Date, idUser: number, idProperty: number, createdAt?: Date, updatedAt?: Date) {
         this.id = id;
         this.title = title;
         this.startDate = startDate;
         this.endDate = endDate;
         this.idUser = idUser;
         this.idProperty = idProperty;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 }
 
