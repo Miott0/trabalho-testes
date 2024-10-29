cebcc3bfc73c7955002136d4cec795a6563a6b32
Maycon-M <maycon.fernandes9@gmail.com>
2024-10-28 21:25:52 -0300

    começando a configura swagger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
-import app from './app';
 
-const port = process.env.PORT || 3000;
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpecs } from './swaggerConfig'; 
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const app = express();
+
+// Middleware para JSON
+app.use(express.json());
+
+// Configuração do Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+
+// Suas outras rotas e middlewares
+//app.use('/appointments', appointmentRoutes);
+//app.use('/properties', propertyRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Swagger disponível em http://localhost:${PORT}/api-docs`);
 });
